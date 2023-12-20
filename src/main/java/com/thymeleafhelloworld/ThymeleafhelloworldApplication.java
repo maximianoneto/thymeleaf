7530ed9dd497a60727525fc264ece839c141c0e6
Maximianocn <maximiano.neto@sou.unifal-mg.edu.br>
2023-12-20 16:47:12 -0300

    first commit

---
+++ src/main/java/com/thymeleafhelloworld/ThymeleafhelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleafhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafhelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafhelloworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thymeleafhelloworld/ThymeleafhelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleafhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafhelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
